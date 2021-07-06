8f789e172935e30bdd611fdd7a2d318b1a74ac71
zakariabijoy <zakaria.bijoy@live.com>
2021-07-06 15:12:08 +0600

    EntityFramework repository demo

--- DapperDemo/Startup.cs
+++ DapperDemo/Startup.cs
@@ -1,4 +1,5 @@
 using DapperDemo.Data;
+using DapperDemo.Repository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -29,6 +30,7 @@ namespace DapperDemo
             {
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
             });
+            services.AddScoped<ICompanyRepository, CompanyRepositoryEF>();
             services.AddControllersWithViews();
         }
 
